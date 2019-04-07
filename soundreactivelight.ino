853868748425beb343ad53b4768fe3886579d09b
jimzhang629 <46616647+jimzhang629@users.noreply.github.com>
2019-04-07 18:45:29 -0500

    Update soundreactivelight.ino

--- soundreactivelight.ino
+++ soundreactivelight.ino
@@ -1,54 +1,134 @@
-/*
-Control LEDs with sound input
-*/
+//www.elegoo.com
+//2016.12.8
 
-int  sensorAnalogPin = A0;    // Select the Arduino input pin to accept the Sound Sensor's analog output 
-int  sensorDigitalPin = 7;    // Select the Arduino input pin to accept the Sound Sensor's digital output
-int  analogValue = 0;         // Define variable to store the analog value coming from the Sound Sensor
-int  digitalValue;            // Define variable to store the digital value coming from the Sound Sensor
-int oldAnalogValue;          // variable to store analog value from previous loop
-                             
-#define RED 11
-#define GREEN 10
-#define BLUE 9
-int redValue;
-int greenValue;
-int blueValue;
+// Define Pins
+#define BLUE 14 //PB6
+#define GREEN 15 //PB7
+#define RED 19 //PB2
+#define delayTime 10
+#include "Ultrasonic.h"
+/* Macro Define */
+
+#define ULTRASONIC_PIN    36             /* pin of the Ultrasonic Ranger */
+#define BUZZER_PIN        39 
+#define LIGHT_SENSOR      26
+/* Global Variables */
+
+Ultrasonic ultrasonic(ULTRASONIC_PIN);    /* Ultrasonic Ranger object */
+int distance = 0;                         /* variable to store the distance to obstacles in front */
+int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
 int i;
+int analog_value = 0;
+/* the setup() method runs once, when the sketch starts */
 
 void setup()
 {
-  Serial.begin(9600);               // The IDE settings for Serial Monitor/Plotter (preferred) must match this speed
-  pinMode(sensorDigitalPin,INPUT);  // Define pin 7 as an input port, to accept digital input
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);
+Serial.begin(9600);
+pinMode(BUZZER_PIN, OUTPUT); 
+pinMode(RED, OUTPUT);
+pinMode(GREEN, OUTPUT);
+pinMode(BLUE, OUTPUT);
+digitalWrite(RED, HIGH);
+digitalWrite(GREEN, HIGH);
+digitalWrite(BLUE, HIGH);
+Serial.println(0);
 }
 
+// define variables
+int redValue;
+int greenValue;
+int blueValue;
 
-void loop(){
-  analogValue = analogRead(sensorAnalogPin); // Read the value of the analog interface A0 assigned to digitalValue
-  digitalValue = digitalRead(sensorDigitalPin); // Read digital value from pin 7 
-  Serial.println("analog");
-  Serial.println(analogValue); // Send the analog value to the serial transmit interface
-  Serial.println("digital");
-  Serial.println(digitalValue);
-  if(analogValue!=oldAnalogValue and digitalValue > 0)      // When the Sound Sensor senses a change in sound, via voltage present, change the LED to a random color
+void playTone(int tone, int duration) 
+{
+  for (long i = 0; i < duration * 1000L; i += tone * 2) 
   {
-      for(i=0;i<9;i++){
-        redValue = random(255);
-        blueValue = random(255);
-        greenValue = random(1);
-        analogWrite(RED,redValue);
-        analogWrite(BLUE,blueValue);
-        analogWrite(GREEN,greenValue);
-        delay(100);
-      }
+    digitalWrite(BUZZER_PIN, HIGH);
+    delayMicroseconds(tone/5);
+    digitalWrite(BUZZER_PIN, LOW);
+    delayMicroseconds(tone/5);
   }
-  oldAnalogValue = analogValue;
-  
-  delay(400);                  // Slight pause so that we don't overwhelm the serial interface
+}
+
+// main loop
+void loop()
+{
+
+distance = ultrasonic.MeasureInCentimeters();   /* read the value from the sensor */  
+Serial.println(distance);
+    i = distance / 10;
+    if (i >= 7){
+      i = 7;
+      }
+    playTone(tones[i], 300);
+    if(tones[i] == 1915){
+      redValue = 250;
+      blueValue = 0;
+      greenValue = 0;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 1700){
+      redValue = 150;
+      blueValue = 0;
+      greenValue = 150;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 1519){
+      redValue = 0;
+      blueValue = 0;
+      greenValue = 250;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 1432){
+      redValue = 0;
+      blueValue = 150;
+      greenValue = 150;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 1275){
+      redValue = 0;
+      blueValue = 250;
+      greenValue = 100;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 1136){
+      redValue = 40;
+      blueValue = 200;
+      greenValue = 50;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 1014){
+      redValue = 100;
+      blueValue = 200;
+      greenValue = 200;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    if(tones[i] == 956){
+      redValue = 250;
+      blueValue = 250;
+      greenValue = 250;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      analogWrite(BLUE, blueValue);
+    }
+    analog_value = analogRead(LIGHT_SENSOR);
+    while (analog_value < 800){
+      delay(30);
+      analog_value = analogRead(LIGHT_SENSOR);
+      }
+      
 }
